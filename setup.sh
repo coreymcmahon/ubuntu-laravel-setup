bdcc70c062ce4ded5d0d158026047fc7e42ea194
Corey McMahon <contact@coreymcmahon.com>
2013-09-22 13:11:04 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+# install apache and php5
 sudo apt-get -y install apache2
 sudo apt-get -y install php5
 sudo apt-get -y install libapache2-mod-php5
+
+# install mysql
 sudo apt-get -y install mysql-server
 sudo apt-get -y install libapache-mod-auth-mysql
 sudo apt-get -y install php5-mysql
+
+# install mcrypt PHP extension
 sudo apt-get -y install php5-mcrypt
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# restart apache
 sudo /etc/init.d/apache2 restart
 
 # install composer
